e6613b5610302f3b8a84b8111d1bc647b0996930
AntoineJouvance <antoinejou94@gmail.com>
2018-04-07 21:38:30 +0200

    2eme commit

--- yo/app/scripts/controllers/main.js
+++ yo/app/scripts/controllers/main.js
@@ -7,11 +7,26 @@
  * # MainCtrl
  * Controller of the yoApp
  */
-angular.module('yoApp')
-  .controller('MainCtrl', function () {
+var api = angular.module('yoApp');
+
+api.controller('MainCtrl', function ($scope,$http) {
+
+    //Récupération des personnes
+    $http.get("http://localhost:8400/API/rest/persons/").then(function (response) {
+        $scope.persons = response.data;
+    })
+
+    $scope.addPerson = function (form) {
+        $http({
+            method: 'POST',
+            url: "http://localhost:8400/API/rest/person?firstname="+$scope.firstname+"&name="+$scope.name+"&mail="+$scope.mail,
+        }).then(function (result) {
+        })
+    }
+
     this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
+        'HTML5 Boilerplate',
+        'AngularJS',
+        'Karma'
     ];
-  });
+});
